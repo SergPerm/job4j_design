2cdef507a7364e59570c1c8eb062b4572a308dd2
Serg Mokhin <svmokhin@gmail.com>
2021-05-03 19:42:56 +0500

    1. Динамический список на массиве. [#526323]

---
+++ src/main/java/ru/job4j/generic/SimpleArrayList.java
@@ -0,0 +1,97 @@
+package ru.job4j.generic;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements Iterable {
+    private Object[] array;
+    private int size;
+    private long modCount;
+
+    public SimpleArrayList() {
+        this.array = new Object[5];
+        this.size = 0;
+        modCount = 0;
+    }
+
+    public SimpleArrayList(int length) {
+        this.array = new Object[length];
+        this.size = 0;
+        modCount = 0;
+    }
+
+    /**
+     * возвращает итератор, предназначенный для обхода данной структуры.
+     * @return - итератор для обхода данной структуры.
+     */
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int point = 0;
+            final long expectedModCount = modCount;
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return point < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) array[point++];
+            }
+        };
+    }
+
+    /**
+     * добавляет указанный элемент (model) в первую свободную ячейку;
+     * @param model -  значение элемента;
+     */
+    public void add(T model) {
+        if (size == array.length) {
+            array = Arrays.copyOf(array, (int) array.length * 2);
+        }
+        array[size++] = model;
+        modCount++;
+    }
+
+    /**
+     * заменяет указанным элементом (model) элемент,
+     * находящийся по индексу index;
+     * @param index - индекс элемента;
+     * @param model - значение элемента;
+     */
+     public void set(int index, T model) {
+         Objects.checkIndex(index, size);
+         array[index] = model;
+     }
+
+    /**
+     * удаляет элемент по указанному индексу;
+     * @param index - индекс элемента;
+     */
+    public void remove(int index) {
+        Objects.checkIndex(index, size);
+        System.arraycopy(array,
+                index + 1,
+                array,
+                index,
+                array.length - index - 1);
+        size--;
+        modCount++;
+    }
+
+    /**
+     * возвращает элемент, расположенный по указанному индексу;
+     * @param index - индекс элемента;
+     * @return - возвращает элемент массива;
+     */
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return (T) array[index];
+    }
+}
---
+++ src/test/java/ru/job4j/generic/SimpleArrayListTest.java
@@ -0,0 +1,67 @@
+package ru.job4j.generic;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimpleArrayListTest {
+
+    @Test
+    public void whenAddThenGet() {
+        SimpleArrayList<String> array = new SimpleArrayList<>();
+        array.add("first");
+        String rsl = array.get(0);
+        assertThat(rsl, is("first"));
+    }
+
+    @Test
+    public void whenAddThenIt() {
+        SimpleArrayList<String> array = new SimpleArrayList<>();
+        array.add("first");
+        String rsl = array.iterator().next();
+        assertThat(rsl, is("first"));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetEmpty() {
+        SimpleArrayList<String> array = new SimpleArrayList<>(10);
+        array.get(0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetOutBound() {
+        SimpleArrayList<String> array = new SimpleArrayList<>();
+        array.add("first");
+        array.get(1);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenGetEmptyFromIt() {
+        SimpleArrayList<String> array = new SimpleArrayList<>();
+        array.iterator().next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenCorruptedIt() {
+        SimpleArrayList<String> array = new SimpleArrayList<>();
+        array.add("first");
+        Iterator<String> it = array.iterator();
+        array.add("second");
+        it.next();
+    }
+
+    @Test
+    public void whenCreated2AndAdded() {
+        SimpleArrayList<String> array = new SimpleArrayList<>(2);
+        array.add("first");
+        array.add("second");
+        array.add("three");
+        String rsl = array.get(2);
+        assertThat(rsl, is("three"));
+    }
+}
