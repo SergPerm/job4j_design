6499a832e96082e1e22da09e758deacfd30298bf
Serg Mokhin <svmokhin@gmail.com>
2021-05-04 22:32:05 +0500

    other

--- src/test/java/ru/job4j/collection/list/SimpleArrayListTest.java
+++ src/test/java/ru/job4j/collection/list/SimpleArrayListTest.java
@@ -1,6 +1,7 @@
-package ru.job4j.generic;
+package ru.job4j.collection.list;
 
 import org.junit.Test;
+import ru.job4j.collection.list.SimpleArrayList;
 
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
