4e280a9881a7f3f4b82bd220ffcd94fc22d0638f
Serg Mokhin <svmokhin@gmail.com>
2021-05-05 19:53:57 +0500

    5.2.2. Реализовать Store<T extends Base> [#526344]

--- src/main/java/ru/job4j/generic/MemStore.java
+++ src/main/java/ru/job4j/generic/MemStore.java
@@ -14,27 +14,55 @@ public final class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public boolean replace(String id, T model) {
-        for (int i = 0; i < mem.size(); i++) {
-            if (mem.get(i).getId().equals(id)) {
-                mem.set(i, model);
-                return true;
-            }
-        }
-        return false;
+        mem.set(findIndexById(id), model);
+        return true;
     }
+
+//    @Override
+//    public boolean replace(String id, T model) {
+//        for (int i = 0; i < mem.size(); i++) {
+//            if (mem.get(i).getId().equals(id)) {
+//                mem.set(i, model);
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
     @Override
     public boolean delete(String id) {
-        T tmp = findById(id);
-        return mem.remove(tmp);
+        mem.remove(findIndexById(id));
+        return true;
     }
+
+//    @Override
+//    public boolean delete(String id) {
+//        T tmp = findById(id);
+//        return mem.remove(tmp);
+//    }
 
     @Override
     public T findById(String id) {
-        T tmp = null;
-        for (T t : mem) {
-            if (t.getId().equals(id)) {
-                tmp = t;
+        return mem.get(findIndexById(id));
+    }
+//
+//    @Override
+//    public T findById(String id) {
+//        T tmp = null;
+//        for (T t : mem) {
+//            if (t.getId().equals(id)) {
+//                tmp = t;
+//                break;
+//            }
+//        }
+//        return tmp;
+//    }
+
+    private int findIndexById(String id) {
+        int tmp = -1;
+        for (int i = 0; i < mem.size(); i++) {
+            if (mem.get(i).getId().equals(id)) {
+                tmp = i;
                 break;
             }
         }
