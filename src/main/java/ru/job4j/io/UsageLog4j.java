bd4193f93c33f2d4f7c8f1ce4331ea6b76353390
Serg Mokhin <svmokhin@gmail.com>
2021-07-01 23:13:35 +0500

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,22 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Serg";
+        int countFriends = 176;
+        float weight = 78.5f;
+        short height = 176;
+        char sex = 'm';
+        boolean isProgrammer = true;
+        double salary = 250000.56;
+        long skillLevel = 100500L;
+        byte age = 45;
+
+//        LOG.trace("trace message");
+        LOG.debug("User name: {}, sex: {}, age {}, height: {}, weight: {}, "
+                + "is a programmer: {}, skillLevel {}, have friends {}, have salary : {}", name,
+                sex, age, height, weight, isProgrammer, skillLevel, countFriends, salary);
+//        LOG.info("info message");
+//        LOG.warn("warn message");
+//        LOG.error("error message");
     }
 }
