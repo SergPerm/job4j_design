8547d7c46398eca47e764b52e32f4ba30b7f44d3
Serg Mokhin <svmokhin@gmail.com>
2021-08-24 22:13:33 +0500

    2. Поиск файлов по критерию [#783] edit1

--- src/main/java/ru/job4j/io/search/Find.java
+++ src/main/java/ru/job4j/io/search/Find.java
@@ -16,7 +16,12 @@ public class Find {
     private static Predicate<Path> getCondition(String nameForSearch, String typeOfNameForSearch) {
         return switch (typeOfNameForSearch.toLowerCase()) {
             case "name" -> p -> p.toFile().getName().equals(nameForSearch);
-            case "mask" -> p -> p.toFile().getName().endsWith(nameForSearch);
+//            case "mask" -> p -> p.toFile().getName().endsWith(nameForSearch);
+            case "mask" -> p -> {
+                Pattern pattern = Pattern.compile(nameForSearch.split("\\.")[1]);
+                Matcher matcher = pattern.matcher(p.toFile().getName());
+                return matcher.find();
+            };
             case "regex" -> p -> {
                 Pattern pattern = Pattern.compile(nameForSearch);
                 Matcher matcher = pattern.matcher(p.toFile().getName());
@@ -24,14 +29,6 @@ public class Find {
             };
             default -> throw new IllegalStateException("Unexpected value: " + typeOfNameForSearch.toLowerCase());
         };
-    }
-
-    private static void writeToFile(Path nameFile) {
-        try (FileWriter fw = new FileWriter(argsName.get("-o"))) {
-            fw.write(nameFile.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
@@ -42,11 +39,38 @@ public class Find {
 
     public static void main(String[] args) throws IOException {
         argsName = ArgsName.of(args);
-        Path start = Paths.get(argsName.get("-d"));
-        search(start, getCondition(argsName.get("-n"), argsName.get("-t"))).forEach(Find::writeToFile);
+        String direction = argsName.get("-d");
+        if (direction == null) {
+            System.out.println("что-то пошло не так, проверьте имя директории");
+            return;
+        }
+        String nameToFind = argsName.get("-n");
+        if (nameToFind == null) {
+            System.out.println("что-то пошло не так, проверьте имя для поиска");
+            return;
+        }
+        String typeOfFind = argsName.get("-t");
+        if (typeOfFind == null) {
+            System.out.println("что-то пошло не так, проверьте тип запроса");
+            return;
+        }
+        String nameFileToResult = argsName.get("-o");
+        if (nameFileToResult == null) {
+            System.out.println("что-то пошло не так, проверьте имя файла для результата");
+            return;
+        }
+        Path start = Paths.get(direction);
+        List<Path> result = search(start, getCondition(nameToFind, typeOfFind));
+        try (FileWriter fw = new FileWriter(nameFileToResult)) {
+            for (Path path : result) {
+                fw.write(path.toString() + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
-//3. Имя файла может задаваться, целиком, по маске, по регулярному выражению(не обязательно).
+//3. Имя файла может задаваться, целиком, по маске, по регулярному выражению(необязательно).
 //        4. Программа должна собираться в jar и запускаться через java -jar find.jar -d=c:/ -n=*.txt -t=mask -o=log.txt
 //        Ключи
 //        -d - директория, в которой начинать поиск.
