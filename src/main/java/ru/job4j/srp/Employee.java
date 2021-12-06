d0d61c66a9525046f8d8ec993baae61acd03ca9e
Serg Mokhin <svmokhin@gmail.com>
2021-12-06 19:52:17 +0500

    1. Отчеты. [#850 #236603]+

--- src/main/java/ru/job4j/srp/Employee.java
+++ src/main/java/ru/job4j/srp/Employee.java
@@ -50,7 +50,9 @@ public class Employee {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
+        if (this == o) {
+            return true;
+        }
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
